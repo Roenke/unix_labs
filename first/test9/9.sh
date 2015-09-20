76c34fe58b67b4ce45f8096204b74a8565f12fa6
Vitaly Bibaev <roenke54@gmail.com>
2015-09-20 23:55:10 +0300

    tasks v2

--- first/test9/9.sh
+++ first/test9/9.sh
@@ -1,1 +1,6 @@
-find ./ -not -iname "*.sh" -type f -mtime -2 | xargs -I x mv x _x
+#/bin/bash
+find ./ -not -iname  "*.sh" -type f -mtime -2 -mtime 1 -print0 | xargs -0 -I x echo x | while read -r x; do
+	dir=$(dirname "$x")
+	base=$(basename "$x")
+	mv "$x" "$dir/_$base"
+done
