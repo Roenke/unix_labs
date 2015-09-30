15d4d99ef0520f9046fb3133114cd7ac0ed04686
Vitaly Bibaev <roenke54@gmail.com>
2015-09-30 12:20:14 +0300

    fix bash commands execute fails

--- lecture_hw2/mycron.py
+++ lecture_hw2/mycron.py
@@ -3,6 +3,7 @@ import time
 import datetime
 import heapq
 import subprocess
+import os
 
 def log(msg):
     log_file = open('/var/mycron.log', 'a')
@@ -14,7 +15,7 @@ def log(msg):
 
 def process():
     tasks_file = open('/etc/mycron.cfg', 'r')
-    # tasks_file = open('./tests/mycron.cfg', 'r')
+    # tasks_file = open('./tests/mycron.cfg', mode='r')
     tasks = list()
     start_time = datetime.datetime.utcnow()
     while True:
@@ -23,7 +24,7 @@ def process():
             break
         t = int(time_str)
         command = tasks_file.readline()
-        tasks.append((start_time + datetime.timedelta(minutes=t), (t, command)))
+        tasks.append((start_time + datetime.timedelta(seconds=t), (t, command)))
         format_msg = 'Successful parse command "{0}". With interval = {1}'
         log(format_msg.format(command, t))
 
@@ -36,8 +37,8 @@ def process():
             command = nearest[1][1]
             t = nearest[1][0]
             log('Execute: {0}'.format(command))
-            subprocess.Popen(command.split())
-            next_time = datetime.datetime.utcnow() + datetime.timedelta(minutes=t)
+            subprocess.Popen(['bash', '-c', command])
+            next_time = datetime.datetime.utcnow() + datetime.timedelta(seconds=t)
             heapq.heappush(tasks, (next_time, nearest[1]))
 
         time.sleep(1)
